f5ae59f08a2d550b90761c22ef4bd9bbf3cbea39
Kevin Incorvia <incorvia@Kevins-MacBook-Air.local>
2012-09-01 21:05:36 -0400

    generated theme class

---
+++ app/models/ryte/theme.rb
@@ -0,0 +1,2 @@
+class Ryte::Theme
+end
---
+++ spec/factories/ryte_themes.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :ryte_theme, :class => 'Ryte::Theme' do
+  end
+end
