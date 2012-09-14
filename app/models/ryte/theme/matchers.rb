939aab4d1e4c58d3f662d2a136f97792bf699c17
Kevin Incorvia <kevin@incorvia.me>
2012-09-14 00:37:52 -0400

    add partials support

--- app/models/ryte/theme/matchers.rb
+++ app/models/ryte/theme/matchers.rb
@@ -4,7 +4,30 @@ class Ryte::Theme::Matchers
     include Rails.application.routes.url_helpers
   end
 
+  # Admin Path
   Braai::Template.map(/({{\s*admin_path\s*}})/i) do |template, key, matches|
     "#{new_admin_session_path}"
+  end
+
+  # Partials
+  Braai::Template.map(/{{(\s*partial\s*(\w*)\s*(\|.*)?)}}/i) do |template, key, matches|
+    path        = File.join(Settings.current_views_path, "_#{matches[1]}.html")
+    partial     = "#{File.open(path).read}"
+    attributes  = {}
+
+    if matches[2]
+      attr_set    = matches[2][1..-1].strip.split(',') if matches[2]
+
+      attr_set.each do |hash|
+        kv_array  = hash.split(":")
+        new_key   = kv_array[0].strip
+        new_val   = kv_array[1].strip
+        new_value = template.attributes[new_val] ||= new_val
+
+        attributes[new_key] = new_value
+      end
+    end
+
+    Braai::Template.new(partial).render(attributes)
   end
 end
--- spec/models/ryte/theme/matchers_spec.rb
+++ spec/models/ryte/theme/matchers_spec.rb
@@ -1,4 +1,72 @@
 require 'spec_helper'
 
 describe Ryte::Theme::Matchers do
+
+  describe 'Admin Path' do
+
+    let(:template) { "{{ admin_path }}" }
+    let(:response) { Braai::Template.new(template).render }
+
+    it 'should return the path to the admin area' do
+      response.should eql("/admins/sign_in")
+    end
+  end
+
+  describe "Partials" do
+
+    context 'no variables' do
+
+      let(:template) { "{{ partial header }}" }
+      let(:response) { Braai::Template.new(template).render }
+
+      it "should render the injected partial" do
+        response.should match(/Header\ Partial/)
+      end
+    end
+
+    context 'one partial variable' do
+
+      let(:post) { create(:ryte_post, body: "body1") }
+      let(:template) { "{{ partial header | foo: post }}" }
+      let(:response) { Braai::Template.new(template).render(name: "Bobby", post: post) }
+
+      it "should render the injected partial" do
+        response.should match(/Header\ Partial/)
+      end
+
+      it "should process braai partial tags" do
+        response.should match(/Bobby/)
+      end
+
+      it "should pass variables as defined in tag" do
+        response.should match(/body1/)
+      end
+    end
+
+    context "multiple partial variables" do
+
+      let(:post) { create(:ryte_post, body: "body1") }
+      let(:post2) { create(:ryte_post, body: "body2") }
+      let(:template) { "{{ partial header | foo: post, foo2: post2 }}" }
+      let(:response) do
+        Braai::Template.new(template).render(name: "Bobby", post: post, post2: post2)
+      end
+
+      it "should render the injected partial" do
+        response.should match(/Header\ Partial/)
+      end
+
+      it "should process braai partial tags" do
+        response.should match(/Bobby/)
+      end
+
+      it "should pass variables as defined in tag" do
+        response.should match(/body1/)
+      end
+
+      it "should pass variables as defined in tag" do
+        response.should match(/body2/)
+      end
+    end
+  end
 end
