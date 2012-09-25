01db6731affd670917293d73d59396986709bfd1
Kevin Incorvia <kevin@incorvia.me>
2012-09-25 08:53:41 -0400

    add statuses and versioning for posts

--- app/models/ryte/post.rb
+++ app/models/ryte/post.rb
@@ -1,7 +1,21 @@
 class Ryte::Post
   include Mongoid::Document
+  include Mongoid::Versioning
   include ActiveModel::ForbiddenAttributesProtection
+
+  max_versions 5
+
+  ALLOWED_STATUSES = %w(draft published)
 
   field :title, type: String, default: ""
   field :body,  type: String, default: ""
+  field :status, type: String, default: ""
+
+  validates :status, inclusion: ALLOWED_STATUSES
+
+  ALLOWED_STATUSES.each do |status|
+    define_method "#{status}?" do
+      self.status == status
+    end
+  end
 end
--- spec/models/ryte/post_spec.rb
+++ spec/models/ryte/post_spec.rb
@@ -1,4 +1,36 @@
 require 'spec_helper'
 
 describe Ryte::Post do
+
+  let(:post) { build(:ryte_post) }
+
+  describe "validations" do
+
+    Ryte::Post::ALLOWED_STATUSES.each do |status|
+      it "should allow '#{status}' as a status" do
+        post.status = status
+        post.should be_valid
+      end
+    end
+
+    it "should dissallow not allowed statuses" do
+      post.status = "foo"
+      post.should_not be_valid
+    end
+  end
+
+  Ryte::Post::ALLOWED_STATUSES.each do |status|
+    describe "#{status}?" do
+
+      it "should return true when status is appropriate" do
+        post.status = status
+        post.send("#{status}?").should be_true
+      end
+
+      it "should return false when inappropriate" do
+        post.status = "foo"
+        post.send("#{status}?").should be_false
+      end
+    end
+  end
 end
